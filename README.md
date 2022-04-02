3193abf90419f26c842382e8543f1a402116ca50
Andrew Byrd <andrew@fastmail.net>
2022-04-02 22:32:07 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Polar H10 Bluetooth Example
+
+This is a basic example of connecting to a Polar H10 bluetooth single-lead ECG device (heart rate monitor). 
+
+I do not have much experience interfacing with Bluetooth devices, so rather than diving into platform-specific calls I wanted to begin with a cross-platform library in Python. The code here was written for the Polar H10 because this is the device I have on hand. It has only been tested with this device, but certain aspects of the code should work with other heart rate devices.
+
+The Polar H10 provides heart rate, R-R interval, and the realtime ECG data from which these figures are derived. The ECG data is filtered on the device to remove low frequency components from muscle movement etc. It can also provide other data such as accelerometer data which are not yet read by this example.
+
+The H10 is a Bluetooth Low Energy (BLE) device, a standard which is actually completely separate from classic Bluetooth. The heart rate and R-R interval data area follow the Bluetooth Low Energy (BLE) Generic Attribute Profile (GATT), which makes them non-proprietary and allows them to be read by devices from various manufacturers without special configuration. The ECG data follows the proprietary Polar Measurement Data (PMD) specification.
+
+## Abbreviations
+- BLE: Bluetooth Low Energy, a separate specification from "classic" Bluetooth.
+- PMD: Polar Measurement Data. This is explained by the PMD Specification document.
+- PPG: Photoplethysmography (PPG) is optical blood flow data, as collected by watches or armbands like the Polar OH1. The Polar H10 collects true ECG data rather than PPG.
+- Other relevant abbreviations are defined _at the end_ of the Polar Measurement Data Specification.
+
+## Bluetooth Low Energy
+Bluetooth Low Energy (BLE) is a completely separate standard from classic Bluetooth. BLE Servers advertise their presence every so often. They may advertise frequently after some kind of interaction, then back off to save energy.
+
+It has a data rate of 100kbit to 1Mbit per second. This is not sufficient for transmitting voice, but fine for many other purposes. It uses very little energy. Some devices like the H10 can run for one or two years on a single cell battery.
+
+BLE has several pairing procedures. Some devices Just Work (this is apparently the technical name), while others rely on passkey entry to flout Man In The Middle (MITM) attacks. The H10 seems to "just work". Connections begin in Security Mode 1, Level 1 (no authentication and no encryption) and can then be upgraded to any security level. [1]
+
+A certain UUID range is reserved for GATT standard services. These are represented with four hex digits (a 16 bit integer), which are substituted into the first 32-bit section of the UUID (e.g. the service `ABCD` gives the UUID `0000ABCD-0000-1000-8000-00805f9b34fb`). All other pseudorandom UUIDs can be used by manufacturers for their own purposes. You shouldn't need to manually insert the 16 bit numbers into this standard UUID (as done in some example code). I'd expect the bluetooth library to do this for you.
+
+Each device has a MAC address and UUID that uniquely identify it. For some reason these are not used to connect to the device on MacOS. A UUID is used instead.
+
+BLE GATT defines some technical terms. It's important when reading technical documentation like the Polar Measurement Data (PMD) Specification to interpret these terms in their narrow GATT technical sense, not as everyday English words. The hierarchy of terms is: Device -> Service -> Characteristic -> Detail. Each one of these has a reserved UUID (in the case of standard GATT) or a pseudorandom one assigned by a manufacturer for proprietary extensions. I will write these terms with Initial Capitals to indicate that they are being used in their narrow technical sense. When reading the PMD specification, note that a list of acronyms is *at the end* of the document, and some important interactions are given only as examples
+
+Heart Rate is a standard Characteristic. The H10 defines an additional Polar Measurement Data (PMD) Service whose UUID begins with `FB005C80`. This contains two Characteristics whose UUIDs are the same as the Service except that the initial 32-bit segments end with `5C81` (the PMD Control Point) and `5C82` (the PMD Data MTU Characteristic).
+
+If the H10 is paired with some other device (such as your phone or watch), you may get `bleak.exc.BleakError: Device with address X was not found`. Or maybe if the contacts are not wet - once I wet them, I saw it three times in 5 seconds. But then it failed again - maybe I need to call stop_notify and disconnect to free it up. Or maybe it was simply Bluetility interfering with my connection attempt.
+
+The H10 seems to advertise its presence even when it's not being worn, but seems to refuse connections until it detects skin contact.
+
+The ECG data is not present in the standard GATT profile and is specific to Polar. It uses a Service and Characteristics Polar Measurement Data (PMD) Specification document, which is available as a PDF in the Polar BLE SDK repository on GitHub.
+
+read_gatt_char means read a Characteristic, not a *character*.
+
+There are no spec pages for querying or setting stream settings or starting a stream, but the document contains examples of doing these things for Acceleration, ECG, and PPG streams with embedded explanations of the bytes in the messages.
+
+
+A tool called Bluetility can be used on MacOS to browse the characteristics of nearby bluetooth devices.
+
+Sources:
+[1](https://medium.com/rtone-iot-security/deep-dive-into-bluetooth-le-security-d2301d640bfc) 
+- https://en.wikipedia.org/wiki/QRS_complex
+- https://en.wikipedia.org/wiki/Bluetooth_Low_Energy#Software_model 
+- [Polar BLE SDK on GitHub](https://github.com/polarofficial/polar-ble-sdk) This is only for mobile (iOS and Android) but may contain some clues in the source code and GitHub issues, and contains PDF reference docs.
+- The GitHub issues such as [this one](https://github.com/polarofficial/polar-ble-sdk/issues/213) contain examples of how to configure the PMD stream.
+- It contains a [sample application](https://github.com/polarofficial/polar-ble-sdk/blob/master/examples/example-android/androidBleSdkTestApp/app/src/main/java/com/polar/androidblesdk/MainActivity.kt) that may give some clues about standard usage patterns
+
+
+`brew install python python-tk`
+then run `idle3` or `python3`
---
+++ biofeed.py
@@ -0,0 +1,198 @@
+import asyncio
+import math
+import os
+import signal
+import sys
+import time
+import pandas as pd
+from bleak import BleakClient
+from bleak import BleakScanner
+from bleak.uuids import uuid16_dict
+import matplotlib.pyplot as plt
+import matplotlib
+
+""" Predefined UUID (Universal Unique Identifier) mapping are based on Heart Rate GATT service Protocol that most
+Fitness/Heart Rate device manufacturer follow (Polar H10 in this case) to obtain a specific response input from
+the device acting as an API """
+
+## Source: https://towardsdatascience.com/creating-a-data-stream-with-polar-device-a5c93c9ccc59
+
+## UUID mapping
+uuid16_dict = {v: k for k, v in uuid16_dict.items()}
+
+## This is the device MAC ID, please update with your device ID (of what, the H10?)
+## Apparently on MacOS this has to be a UUID not a MAC address
+ADDRESS = "D4:52:48:88:EA:04"
+
+# Polar Measurement Data (PMD) Service and Characteristics
+# See "Polar Measurement Data Specification for 3rd Party"
+# section "Gatt Service and Characteristics Declaration"
+PMD_UUID_TEMPLATE = "FB00{0:x}-02E7-F387-1CAD-8ACD2D8DF0C8"
+
+# UUID for the Polar Measurement Data Service
+PMD_SERVICE = PMD_UUID_TEMPLATE.format(0x5C80)
+
+## UUID to write or read stream settings
+PMD_CONTROL = PMD_UUID_TEMPLATE.format(0x5C81)
+
+## UUID for the data stream itself, to subscribe to notifications
+PMD_DATA = PMD_UUID_TEMPLATE.format(0x5C82)
+
+# This standard GATT template should be handled by Bleak right? Why do we need to manually substitute it?
+TEMPLATE="0000{0:x}-0000-1000-8000-00805f9b34fb"
+
+## UUID for model number
+MODEL_NBR_UUID = TEMPLATE.format(uuid16_dict.get("Model Number String"))
+
+## UUID for manufacturer name ##
+MANUFACTURER_NAME_UUID = TEMPLATE.format(uuid16_dict.get("Manufacturer Name String"))
+
+## UUID for battery level ##
+BATTERY_LEVEL_UUID = TEMPLATE.format(uuid16_dict.get("Battery Level"))
+
+# Document this "write string".
+
+## UUID for Request of ECG Stream ##
+ECG_WRITE = bytearray([0x02, 0x00, 0x00, 0x01, 0x82, 0x00, 0x01, 0x01, 0x0E, 0x00])
+
+# Plolar H10  sampling frequency (check SDK code for other valid values)
+ECG_SAMPLING_FREQ = 130
+
+def flag(byte, n):
+    return (byte & (1 << n)) != 0
+
+class PolarFeatures:
+    # See "read features from device" example in PMD specification document.
+    def __init__(self, bytes):
+        # Check that this is a "control point feature read response"
+        assert bytes[0] == 0x0F
+        # Read bitfield showing what features the connected device has.
+        # See PMD Measurement Types section of PMD specification document.
+        byte = bytes[1]
+        self.ecg = flag(byte, 0);
+        self.ppg = flag(byte, 1);
+        self.acceleration = flag(byte, 2);
+        self.pp_interval = flag(byte, 3);
+        # bit 4 reserved for future use
+        self.gyroscope = flag(byte, 5);
+        self.magnetometer = flag(byte, 6);
+
+## Resource allocation for data collection
+ecg_session_data = []
+ecg_session_time = []
+
+# A series of three-byte integers in millivolts
+def convert_ecg_data(sender, data):
+    print("ECG data:", data[:10]) #data)
+    if data[0] == 0x00:
+       timestamp = convert_to_unsigned_long(data, 1, 8)
+       step = 3
+       samples = data[10:]
+       offset = 0
+       while offset < len(samples):
+           ecg = convert_array_to_signed_int(samples, offset, step)
+           # print(ecg)    
+           offset += step
+           ecg_session_data.extend([ecg])
+           ecg_session_time.extend([timestamp])
+           
+def convert_array_to_signed_int(data, offset, length):
+    return int.from_bytes(bytearray(data[offset : offset + length]),       
+          byteorder="little", signed=True,)
+          
+def convert_to_unsigned_long(data, offset, length):
+    return int.from_bytes(bytearray(data[offset : offset + length]),   
+          byteorder="little", signed=False,)
+          
+def detection_callback(device, advertisement_data):
+    print(device.address, "RSSI:", device.rssi, "Name:", device.name)
+    # print(device.address, "RSSI:", device.rssi, advertisement_data)
+
+## This is sort-of decoding the PPI Frame Type from the Polar docs (the P-P interval similar to R-R interval), 
+## which I think is not actually supported on the H10, but is identically structured to
+## the GATT heart rate service (except that the latter has a flags field at the beginning)
+def convert_hr_data(sender, data):
+    ## Flags 0-3 are false, flag 4 (R-R interval) is true.
+    ## This means we get an 8-bit heart rate followed by a 16-bit R-R interval
+    if data[0] == 0x16:
+        print("rate:", data[1])
+        print("interval:", convert_array_to_signed_int(data, 2, 2))
+    
+async def callbackScan():
+    scanner = BleakScanner()
+    scanner.register_detection_callback(detection_callback)
+    await scanner.start()
+    await asyncio.sleep(5.0)
+    await scanner.stop()
+    for d in scanner.discovered_devices:
+        print(d)
+
+H10_ADDRESS = "5C92E382-43DC-466E-ADD3-71C1B196E41D"
+
+async def scanPrint():
+    print("Scanning for Polar H10...")
+    devices = await BleakScanner.discover()
+    address = None
+    rssi = -90
+    for device in devices:
+        if device.name.startswith("Polar H10") and device.rssi > rssi:            
+            address = device.address
+            rssi = device.rssi
+    print("Address of H10:", address)
+    async with BleakClient(address) as client:
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        manufacturer = await client.read_gatt_char(MANUFACTURER_NAME_UUID)
+        battery_level = await client.read_gatt_char(BATTERY_LEVEL_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
+        print("Manufacturer: {0}".format("".join(map(chr, manufacturer))))
+        print("Battery Level: {0}".format("".join(map(chr, battery_level))))
+
+# asyncio.run(scanPrint())
+
+## UUID for heart rate
+##https://stackoverflow.com/questions/69977624/how-do-i-find-out-which-uuid-i-should-use-to-request-data-from-my-polar-h10-sens
+## also
+##https://stackoverflow.com/questions/52970763/trying-to-get-heart-rate-variability-from-polar-h10-bluetooth-low-energy-sample
+## These are standard bluetooth heart rate items, see bluetooth docs.
+
+## GATT Characteristic and Object Type 0x2A37 is "Heart Rate Measurement"
+HR_UUID = TEMPLATE.format(0x2A37)
+## Bluetooth assigned UUID document also specifies GATT service 0x180D which yields "characteristic not found"
+
+## ECG data is a PMD_DATA stream.
+## Heart Rate and RR interval are apparently not available as PMD_DATA streams on the H10, 
+## despite what the PDF docs say.
+
+## https://github.com/hbldh/bleak/issues/786 implies there is some problem 
+## with write_gatt_char but I haven't seen it. NOTE that issue contains a great "minimum working example"
+
+async def info():
+    # When we exit this scope it will close the client 
+    # (though confusingly the variable doesn't go out of scope)
+    async with BleakClient(H10_ADDRESS) as client:
+        services = await client.get_services()
+        print("Available services:")
+        for service in services:
+            print(service)
+        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
+        manufacturer = await client.read_gatt_char(MANUFACTURER_NAME_UUID)
+        battery_level = await client.read_gatt_char(BATTERY_LEVEL_UUID)
+        print("Model Number: {0}".format("".join(map(chr, model_number))))
+        print("Manufacturer: {0}".format("".join(map(chr, manufacturer))))
+        print("Battery Level: {0}%".format(battery_level[0]))    
+        att_read = await client.read_gatt_char(PMD_CONTROL)
+        print("Polar Measurement Data features:", vars(PolarFeatures(att_read)))
+        await client.write_gatt_char(PMD_CONTROL,  ECG_WRITE)
+        ## Request notifications of ECG stream
+        await client.start_notify(PMD_DATA, convert_ecg_data)
+        ## Also start a heart rate notification stream
+        await client.start_notify(HR_UUID, convert_hr_data)
+        await asyncio.sleep(5.0)
+        await client.stop_notify(PMD_DATA)
+        await client.stop_notify(HR_UUID)
+        await client.disconnect()
+
+
+asyncio.run(info())
+#asyncio.run(callbackScan())
+#asyncio.run(scanPrint())
